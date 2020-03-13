f03dcef5e76104135fc94a9d839e1eec3334587c
Cristhian Suriel <42849811+CristhianSuriel@users.noreply.github.com>
2020-03-13 14:59:48 -0400

    added Jehnna's changes

--- sqlite_run.py
+++ sqlite_run.py
@@ -17,38 +17,38 @@ c.execute("""CREATE TABLE tickets(
 
 
 # This function will add a guests when they subscribe on a membership
-def add_member(emp):
+def add_member(customer):
     with conn:
         c.execute("INSERT INTO membership VALUES (:first, :last, :email)",
-                  {'first': emp.first, 'last': emp.last, 'email': emp.email})
+                  {'first': customer.first, 'last': customer.last, 'email': customer.email})
         c.execute("INSERT INTO tickets VALUES (:email, :ticket_number)",
-                  {'email': emp.email, 'ticket_number': emp.ticket_number})
+                  {'email': customer.email, 'ticket_number': customer.ticket_number})
 
 
-def get_member_by_name(last_name):
-    c.execute("SELECT * FROM employees WHERE last=:last",
-              {'last': last_name})
+def get_member_by_name(first_name, last_name):
+    c.execute("SELECT * FROM membership WHERE last=:last and first=:first",
+              {'last': last_name, 'first': first_name})
     return c.fetchall()
 
 
 # This function will allow members to update their email address
-def update_email(emp, email):
-    temp = emp.email
+def update_email(customer, email):
+    temp = customer.email
     with conn:
         c.execute("""UPDATE membership SET email = :email
                     WHERE first = :first and last = :last""",
-                  {'first': emp.first, 'last': emp.last, 'email': email})
-
-        c.execute("""UPDATE tickets SET email = :email
-                    WHERE temp = :temp""",
-                  {'email': email, 'temp': temp})
+                  {'first': customer.first, 'last': customer.last, 'email': email})
+        #
+        # c.execute("""UPDATE tickets SET email = :email
+        #             WHERE temp = :temp""",
+        #           {'email': email, 'temp': temp})
 
 
 # This adds to the ticket table every new ticket purchased
-def purchase_ticket(emp, ticket_number):
+def purchase_ticket(customer, ticket_number):
     with conn:
         c.execute("INSERT INTO tickets VALUES (:email, :ticket_number)",
-                  {'email': emp.email, 'ticket_number': ticket_number})
+                  {'email': customer.email, 'ticket_number': ticket_number})
 
 
 # This shows all tickets bought by someone
@@ -58,16 +58,22 @@ def ticket_history(email):
     return c.fetchall()
 
 
+# adds two guests, Jehnna and John
 guest_1 = Guests('Jehnna', 'Smith', 'js@gmail.com', 1)
 guest_2 = Guests('John', 'Doe', 'jd@gmail.com', 2)
 add_member(guest_1)
 add_member(guest_2)
 
+# John purchases another movie ticket
 purchase_ticket(guest_2, 3)
+# displays John's ticket history
+guest_2_history = ticket_history('jd@gmail.com')
+for num in range(len(guest_2_history)):
+    print(guest_2_history[num])
 
-guest_2_lookup = ticket_history('jd@gmail.com')
-
-for num in range(len(guest_2_lookup)):
-    print(guest_2_lookup[num])
-
+# prints out jehnna's membership info before and after email change
+print(get_member_by_name('Jehnna', 'Smith'))
+update_email(guest_1, 'djehnna@gmail.com')
+print(get_member_by_name('Jehnna', 'Smith'))
+# closes connection
 conn.close()
